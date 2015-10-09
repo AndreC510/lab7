55c4d3928aa465233575d9c1fe783a36bed035fb
Andre <andrectheasianman@gmail.com>
2015-10-09 08:37:24 -0700

    second checkin

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -5,10 +5,6 @@ import java.util.Iterator;
 */
 public interface AddressBook {
 
-   /** 
-       Load the dictionary from a source.
-       @param sourceName the name of the source
-   */
    void load(String sourceName);
 
    /** 
